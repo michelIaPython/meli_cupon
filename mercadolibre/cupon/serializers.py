c21c6bf55fc24ffe6769c4bd6ba4dfe530cb40a6
luffy <michel.estrada13@gmail.com>
2022-07-06 12:25:52 -0500

    document serializers

--- mercadolibre/cupon/serializers.py
+++ mercadolibre/cupon/serializers.py
@@ -6,6 +6,10 @@ from cupon.models import CuponModel
 
 
 class CuponSerializer(serializers.ModelSerializer):
+    """
+    This is the serializer of the API and return only two fields item_id, quantity
+    """
+
     class Meta:
         model = CuponModel
         fields = ("item_id", "quantity")
