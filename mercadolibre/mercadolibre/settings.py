e40e9ce4414c9251126ec99e04656959dcd3cb0f
luffy <michel.estrada13@gmail.com>
2022-07-11 13:43:47 -0500

    add .env to django

--- mercadolibre/mercadolibre/settings.py
+++ mercadolibre/mercadolibre/settings.py
@@ -14,10 +14,14 @@ from pathlib import Path
 
 # import environ
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+# SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x=at^r^1b_+=cfc&e&1b02y)=m9&1_y2=%vmtztcijg5#2k26a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -87,27 +91,14 @@ WSGI_APPLICATION = "mercadolibre.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-"""DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}"""
-
-"""POSTGRESQL_NAME = mercadolibre
-POSTGRESQL_USER = meli
-POSTGRESQL_PASS = 123
-POSTGRESQL_HOST = localhost
-POSTGRESQL_PORT = 5432"""
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mercadolibre",  # os.environ.get("POSTGRESQL_NAME"),  # env("POSTGRESQL_NAME"),
-        "USER": "meli",  # os.environ.get("POSTGRESQL_USER"),
-        "PASSWORD": "123",  # os.environ.get("POSTGRESQL_PASS"),
-        "HOST": "localhost",  # os.environ.get("POSTGRESQL_HOST"),
-        "PORT": "5432",  # os.environ.get("POSTGRESQL_PORT"),
+        "NAME": os.environ.get("POSTGRESQL_NAME"),
+        "USER": os.environ.get("POSTGRESQL_USER"),
+        "PASSWORD": os.environ.get("POSTGRESQL_PASS"),
+        "HOST": os.environ.get("POSTGRESQL_HOST"),
+        "PORT": os.environ.get("POSTGRESQL_PORT"),
     }
 }
 
